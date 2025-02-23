2556af474512e4ad12cc185288050f45ebc4c2a5
Dsb <paraderdsb@gmail.com>
2025-02-23 15:09:21 +0530

    created simple form related product and install prop types

--- src/Comonents/Home/ProduuctForm/ProductForm.jsx
+++ src/Comonents/Home/ProduuctForm/ProductForm.jsx
@@ -1,81 +1,24 @@
+import InputFields from "../../Common/Form/InputFields.jsx";
+
 const ProductForm = () => {
   return (
-    <>
+    <div className="mt-10">
       <form className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
-            placeholder="name@flowbite.com"
-            required
-          />
+        <div className="mb-8 font-bold font-large">
+          <h1>Product Basic Details</h1>
         </div>
-        <div className="mb-5">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="repeat-password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Repeat password
-          </label>
-          <input
-            type="password"
-            id="repeat-password"
-            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
-            required
-          />
-        </div>
-        <div className="flex items-start mb-5">
-          <div className="flex items-center h-5">
-            <input
-              id="terms"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
-            />
-          </div>
-          <label
-            htmlFor="terms"
-            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            I agree with the{" "}
-            <a
-              href="#"
-              className="text-blue-600 hover:underline dark:text-blue-500"
-            >
-              terms and conditions
-            </a>
-          </label>
-        </div>
+          <InputFields type="text" name="product_name" labelName="Product Name" />
+          <InputFields type="number" name="product_quantity" labelName="Product Quantity" />
+          <InputFields type="number" name="product_price" labelName="Product Price" />
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Register new account
+          Create New Product
         </button>
       </form>
-    </>
+    </div>
   );
 };
 export default ProductForm;
